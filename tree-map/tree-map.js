9111ab5a3b8e256629a17df9ff1032d813b55eb3
Shruti Agrawal <shruti98932@gmail.com>
2021-06-19 20:28:08 -0400

    SelfAssessment Completed

--- tree-map/tree-map.js
+++ tree-map/tree-map.js
@@ -28,10 +28,50 @@
   *  root1.value // still 1
   */
 
+// Implement a map method and other(if needed) on tree class
+// map has one argument which is a mapping funcion and it generates a newTree with result;
+//
 var Tree = function(value) {
   this.value = value;
   this.children = [];
 };
+
+Tree.prototype.addChild = function(value){
+ console.log(value);
+ //pushing the values to the roots children
+ this.children.push(value);
+};
+
+Tree.prototype.map = function(mapping){
+//for every root proprty's value, pass it to the cb
+for(var key in this){
+  mapping(key);
+}
+
+};
+
+var root1 = new Tree(1);
+console.log(root1);
+var branch2 = root1.addChild(2);
+console.log(branch2);
+var branch3 = root1.addChild(3);
+console.log(branch3);
+var leaf4 = branch2.addChild(4);
+var leaf5 = branch2.addChild(5);
+var leaf6 = branch3.addChild(6);
+var leaf7 = branch3.addChild(7);
+var newTree = root1.map(function (value) {
+return value * 2;
+})
+console.log(newTree.value) // 2
+console.log(newTree.children[0].value) // 4
+console.log(newTree.children[1].value) // 6
+console.log(newTree.children[0].children[1].value )// 10
+console.log(newTree.children[1].children[1].value) // 14
+console.log(root1.value) // still 1
+
+
+
 
 
 
